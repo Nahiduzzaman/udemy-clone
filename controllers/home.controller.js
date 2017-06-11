81165a9ca7bd357a9f012cf395c3bde2b5c6ddd7
SELISE\nahid.uzzaman <nahid.uzzaman@selise.ch>
2017-06-11 18:05:31 +0600

    added and many changes

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -3,9 +3,10 @@
 	
 	angular.module('starterApp')
 		.controller('homeController', constructor);
-		    constructor.$inject = ['$stateParams','SampleService','$scope','$window','$state'];
-		    function constructor($stateParams,SampleService,$scope,$window,$state) {
-		    	var vm = this;			
+		    constructor.$inject = ['$stateParams','SampleService','$scope','$window','$state','$mdMedia'];
+		    function constructor($stateParams,SampleService,$scope,$window,$state,$mdMedia) {
+		    	var vm = this;
+				vm.$mdMedia = $mdMedia;			
 			}
 
 })(window.angular);
